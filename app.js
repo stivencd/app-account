56d9fd907c66505c544c1066e296f23b198bc967
Stiven Castellon <stiven.castellon@nuevatel.com>
2022-10-04 20:34:26 -0800

    metrics

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ const app = express()
 const appPromise = require('./src/app/middlewares/configprovider').appPromise
 const { ZIPKIN_LOCAL_ENDPOINT, ZIPKIN_SERVICE_NAME } = require('./src/app/common/constants')
 
+const client = require('prom-client');
+const counter = new client.Counter({
+    name: 'http_request_counter_health',
+    help: 'Metrics to count hits on the health api ',
+});
+const register = new client.Registry();
+register.registerMetric(counter)
+register.setDefaultLabels({
+    app: 'app-account',
+    prefix: 'node_'
+  })
+client.collectDefaultMetrics({ register })
+
+
 appPromise.then(function(app) {
     const PORT = process.env.SERVER_PORT_ACCOUNT || 3001
 
@@ -38,6 +52,16 @@ appPromise.then(function(app) {
    app.use(zipkinMiddleware({ tracer }))
    
     app.use('/api', require('./src/app/routes'))
+
+    app.get('/metrics', async (req, res) => {
+        res.setHeader('Content-Type', register.contentType)
+        res.send(await register.metrics())
+    });
+    app.get('/health', async (req, res) => {
+        counter.inc()
+        res.send({message: 'Account App healty'});
+    });
+
     app.listen(PORT, () => {
         console.log('Application running on port ', PORT)
     })
